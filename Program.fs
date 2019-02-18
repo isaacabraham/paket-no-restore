2cc7c41cbd6fbb08551f6649709f0780bc7591b3
Isaac Abraham <isaac.abraham@gmail.com>
2019-02-18 16:17:33 +0100

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about Spurs at https://www.tottenhamhotspur.com/
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "COYS!"
+    1963 // first British team to win UEFA cup
